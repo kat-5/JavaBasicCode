f9e4a80aacb8e2d415b16bfaa2e6882ab97b37c7
kat-5 <catalinaolend@gmail.com>
2022-06-01 18:30:54 -0700

    modified the git class04

---
+++ src/gitClass/GitClass04.java
@@ -0,0 +1,12 @@
+package gitClass;
+
+public class GitClass04 {
+
+	public static void main(String[] args) {
+		
+     System.out.println("pop culture");
+     System.out.println("changes");
+
+	}
+
+}
